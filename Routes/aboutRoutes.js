1a1a491f6116d6b2f0cdddb8f2462bc4b96a3d30
Mani Ram Madu <105063979+Maniram6@users.noreply.github.com>
2025-11-15 12:32:07 +0530

    tkno-1/basic-express (#3)

--- Routes/aboutRoutes.js
+++ Routes/aboutRoutes.js
@@ -18,30 +18,30 @@ router.get('/', (req,res)=> {
 router.get('/:id',(req,res) => {
     
 
-    let index =0;
+//     let index =0;
 
-    for(let i=0; i< Object.keys(req.body).length; i++){
-        if ('"' + req.params.id +  '"' === JSON.stringify(req.body[i].name)) {
-            index = i;
-        }    
-    }
+//     for(let i=0; i< Object.keys(req.body).length; i++){
+//         if ('"' + req.params.id +  '"' === JSON.stringify(req.body[i].name)) {
+//             index = i;
+//         }    
+//     }
 
-   if (index != 0) {
-        res.status(200).send(`Please find below user details of username : ${req.params.id}
-        ${JSON.stringify(req.body[index])}`);
-    }else{
-        res.status(200).send(`Mentioned user not found!`);
-    }
+//    if (index != 0) {
+//         res.status(200).send(`Please find below user details of username : ${req.params.id}
+//         ${JSON.stringify(req.body[index])}`);
+//     }else{
+//         res.status(200).send(`Mentioned user not found!`);
+//     }
 
 
-    // setTimeout(() => {
-    //     for(let i=0; i< Object.keys(req.body).length; i++){
-    //         if ('"' + req.params.id +  '"' === JSON.stringify(req.body[i].name)) {
-    //             res.status(200).send(`Please find below user details of username : ${req.params.id}
-    //             ${JSON.stringify(req.body[i])}`)
-    //         }
-    //     }
-    // }, 0);
+    setTimeout(() => {
+        for(let i=0; i< Object.keys(req.body).length; i++){
+            if ('"' + req.params.id +  '"' === JSON.stringify(req.body[i].name)) {
+                res.status(200).send(`Please find below user details of username : ${req.params.id}
+                ${JSON.stringify(req.body[i])}`)
+            }
+        }
+    }, 0);
 
 
 
